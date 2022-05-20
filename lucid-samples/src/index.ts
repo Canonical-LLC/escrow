f640b6aafd0f5309a98d144f78e76ba1a683465f
Jonathan Fischoff <jonathangfischoff@gmail.com>
2022-05-20 09:00:02 -0700

    move to 2.8. use map

--- lucid-samples/src/index.ts
+++ lucid-samples/src/index.ts
@@ -1,32 +1,29 @@
-import { Construct, Data } from 'lucid-cardano';
+import { Construct, Data, PlutusData } from 'lucid-cardano';
 
-export const buyDatum: Data = Data.from(
+export const buyDatum: Data = Data.to(
   new Construct(1, [
     new Construct(0, []),
     new Construct(0, [
       new Construct(0, [
         '32de4124615eb739d59c5c733d1ccf1e0526ea87076770e0b7ba469a',
-        {
-          d6cfdbedd242056674c0e51ead01785497e3a48afbbb146dc72ee1e2: {
-            '123456': BigInt(1),
-          },
-        },
+        new Map<string, PlutusData>([
+          [
+            'd6cfdbedd242056674c0e51ead01785497e3a48afbbb146dc72ee1e2',
+            new Map<string, PlutusData>([['123456', BigInt(1)]]),
+          ],
+        ]),
         [
           new Construct(0, [
             '32de4124615eb739d59c5c733d1ccf1e0526ea87076770e0b7ba469a',
-            {
-              '': {
-                '': BigInt(49000000),
-              },
-            },
+            new Map<string, PlutusData>([
+              ['', new Map<string, PlutusData>([['', BigInt(49000000)]])],
+            ]),
           ]),
           new Construct(0, [
             '02eae675ca7263105cef4afde685c21bbe80f23ea774b5a0b8834823',
-            {
-              '': {
-                '': BigInt(1000000),
-              },
-            },
+            new Map<string, PlutusData>([
+              ['', new Map<string, PlutusData>([['', BigInt(1000000)]])],
+            ]),
           ]),
         ],
         BigInt(1648849688000),
@@ -37,11 +34,12 @@ export const buyDatum: Data = Data.from(
         [
           new Construct(0, [
             '895ad7cdb46e6c251bc42724640dd68ff62dff291503ad18b6164e84',
-            {
-              d6cfdbedd242056674c0e51ead01785497e3a48afbbb146dc72ee1e2: {
-                '123456': BigInt(1),
-              },
-            },
+            new Map<string, PlutusData>([
+              [
+                'd6cfdbedd242056674c0e51ead01785497e3a48afbbb146dc72ee1e2',
+                new Map<string, PlutusData>([['123456', BigInt(1)]]),
+              ],
+            ]),
           ]),
         ],
       ]),
@@ -49,31 +47,28 @@ export const buyDatum: Data = Data.from(
   ]),
 );
 
-export const sellDatum: Data = Data.from(
+export const sellDatum: Data = Data.to(
   new Construct(0, [
     new Construct(0, [
       '32de4124615eb739d59c5c733d1ccf1e0526ea87076770e0b7ba469a',
-      {
-        d6cfdbedd242056674c0e51ead01785497e3a48afbbb146dc72ee1e2: {
-          '123456': BigInt(1),
-        },
-      },
+      new Map<string, PlutusData>([
+        [
+          'd6cfdbedd242056674c0e51ead01785497e3a48afbbb146dc72ee1e2',
+          new Map<string, PlutusData>([['123456', BigInt(1)]]),
+        ],
+      ]),
       [
         new Construct(0, [
           '32de4124615eb739d59c5c733d1ccf1e0526ea87076770e0b7ba469a',
-          {
-            '': {
-              '': BigInt(49000000),
-            },
-          },
+          new Map<string, PlutusData>([
+            ['', new Map<string, PlutusData>([['', BigInt(49000000)]])],
+          ]),
         ]),
         new Construct(0, [
           '02eae675ca7263105cef4afde685c21bbe80f23ea774b5a0b8834823',
-          {
-            '': {
-              '': BigInt(1000000),
-            },
-          },
+          new Map<string, PlutusData>([
+            ['', new Map<string, PlutusData>([['', BigInt(1000000)]])],
+          ]),
         ]),
       ],
       BigInt(1648849688000),
@@ -82,7 +77,7 @@ export const sellDatum: Data = Data.from(
   ]),
 );
 
-export const cancelRedeemer: Data = Data.from(
+export const cancelRedeemer: Data = Data.to(
   new Construct(2, [
     new Construct(0, [
       '895ad7cdb46e6c251bc42724640dd68ff62dff291503ad18b6164e84',
@@ -91,7 +86,7 @@ export const cancelRedeemer: Data = Data.from(
   ]),
 );
 
-export const buyerApprovesRedeemer: Data = Data.from(
+export const buyerApprovesRedeemer: Data = Data.to(
   new Construct(2, [
     new Construct(0, [
       '895ad7cdb46e6c251bc42724640dd68ff62dff291503ad18b6164e84',
@@ -100,7 +95,7 @@ export const buyerApprovesRedeemer: Data = Data.from(
   ]),
 );
 
-export const mediatorApprovesRedeemer: Data = Data.from(
+export const mediatorApprovesRedeemer: Data = Data.to(
   new Construct(2, [
     new Construct(0, [
       '895ad7cdb46e6c251bc42724640dd68ff62dff291503ad18b6164e84',
